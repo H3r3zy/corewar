3eaddc806af976432bbc2dc71133761e988fd2b1
sahel <sahel.lucas-saoudi@epitech.eu>
2017-03-26 19:12:43 +0200

    merge 100 %

--- asm/src/recup_lines.c
+++ asm/src/recup_lines.c
@@ -5,7 +5,7 @@
 ** Login   <maximilien.desnos@epitech.eu>
 **
 ** Started on  Sat Mar 25 15:07:35 2017 maximilien desnos
-** Last update Sun Mar 26 20:40:20 2017 maximilien desnos
+** Last update Sun Mar 26 18:54:56 2017 Sahel Lucas--Saoudi
 */
 
 #include	<stdlib.h>
@@ -40,6 +40,18 @@ void		find_info(char *fd, t_line *op)
     {
       set_byte(op);
       remp_int(op);
+      //printf(":%d:\n", op->op.code);
+      write(1, &op->op.code, sizeof(char));
+      if (op->have_cb == 1)
+	write(1, &op->cb, sizeof(char));
+      int i = 0;
+      while (op->arg[i])
+	{
+	  //printf("new arg\n"); 
+	  write(1, &op->ret[i], op->byte[i]);
+	  i++;
+	}
+      //printf("bite\n");
     }
 }
 

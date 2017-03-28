70cd22ff72729a14349ed65e00be4d3c0fd36f7e
Desnos <maximilien.desnos@epitech.eu>
2017-03-28 13:11:58 +0200

    FUSION YAHAHA

--- asm/src/reverse_add.c
+++ asm/src/reverse_add.c
@@ -5,19 +5,19 @@
 ** Login   <maximilien.desnos@epitech.eu>
 **
 ** Started on  Tue Mar 28 12:01:16 2017 maximilien desnos
-** Last update Tue Mar 28 12:55:35 2017 maximilien desnos
+** Last update Tue Mar 28 13:10:15 2017 maximilien desnos
 */
 
 unsigned int		reverse_add2(unsigned int i)
 {
-  unsigned int		tmp;
+  unsigned short	tmp;
   char			*in;
   char			*out;
   int			j;
 
   in = (char *)&i;
   out = (char *)(&tmp + 1) - 1;
-  j = sizeof(short);
+  j = sizeof(int);
   while (j > 0)
     {
       *out-- = *in++;

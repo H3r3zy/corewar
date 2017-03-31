05d46c7dd404bf1246c37249d6f47d9ff30b2923
Desnos <maximilien.desnos@epitech.eu>
2017-03-31 21:03:01 +0200

    add sub

---
+++ src/vm/instructions/add_m.c
@@ -0,0 +1,34 @@
+/*
+** add_m.c for corewar in /home/desnosm/rendu/Progelem/CPE_2016_corewar
+**
+** Made by maximilien desnos
+** Login   <maximilien.desnos@epitech.eu>
+**
+** Started on  Fri Mar 31 19:59:16 2017 maximilien desnos
+** Last update Fri Mar 31 21:02:16 2017 maximilien desnos
+*/
+
+#include	<sys/types.h>
+#include	<unistd.h>
+#include	"vm.h"
+
+void		add_m(t_player *player, t_action *actual)
+{
+  char		i;
+  char		j;
+  char		k;
+
+  (void)actual;
+  lseek(player->fd, COMMENT_LENGTH + PROG_NAME_LENGTH + 16 + actual->pos + 1, SEEK_SET);
+  read(player->fd, &i, sizeof(i));
+  read(player->fd, &j, sizeof(j));
+  read(player->fd, &k, sizeof(k));
+  player->reg[k] = player->reg[j] + player->reg[i];
+  if (player->reg[k] == 0)
+    {
+      if (player->carry == 0)
+	player->carry = 1;
+      else if (player->carry == 1)
+	player->carry = 0;
+    }
+}
---
+++ src/vm/instructions/sub_m.c
@@ -0,0 +1,34 @@
+/*
+** sub_m.c for corewar in /home/desnosm/rendu/Progelem/CPE_2016_corewar
+**
+** Made by maximilien desnos
+** Login   <maximilien.desnos@epitech.eu>
+**
+** Started on  Fri Mar 31 20:49:24 2017 maximilien desnos
+** Last update Fri Mar 31 21:02:13 2017 maximilien desnos
+*/
+
+#include	<sys/types.h>
+#include	<unistd.h>
+#include	"vm.h"
+
+void		sub_m(t_player *player, t_action *actual)
+{
+  char		i;
+  char		j;
+  char		k;
+
+  (void)actual;
+  lseek(player->fd, COMMENT_LENGTH + PROG_NAME_LENGTH + 16 + actual->pos + 1, SEEK_SET);
+  read(player->fd, &i, sizeof(i));
+  read(player->fd, &j, sizeof(j));
+  read(player->fd, &k, sizeof(k));
+  player->reg[k] = player->reg[j] - player->reg[i];
+  if (player->reg[k] == 0)
+    {
+      if (player->carry == 0)
+	player->carry = 1;
+      else if (player->carry == 1)
+	player->carry = 0;
+    }
+}
